4f4d09443329730993adf1b11d3221c74b42e5ea
Marc <marcdsp@gmail.com>
2022-12-29 12:39:09 -0600

    Exercises done, struggled on removefrom/sumall

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+const tempC = Math.round(((tempF-32)*(5/9)) * 10) / 10;
+return tempC;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempC) {
+  return tempF = Math.round(((tempC*(9/5))+32) * 10) / 10;
+
+const convertToKelvin = function(tempC) {
 
 // Do not edit below this line
 module.exports = {
